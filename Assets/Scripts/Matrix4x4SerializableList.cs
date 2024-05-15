87800e5df7f05a0beff3bd47289acbc6cd814bad
Jinhoon Choi <62126566+Argoneon1810@users.noreply.github.com>
2024-05-15 15:00:51 +0900

    reverted name change due to incompatibility in python script (if you like to change, in both script the 'matrixList' should be changed into 'data')

--- Assets/Scripts/Matrix4x4SerializableList.cs
+++ Assets/Scripts/Matrix4x4SerializableList.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 [System.Serializable]
 public class Matrix4x4SerializableList
 {
-    public List<Matrix4x4Serializable> data = new List<Matrix4x4Serializable>();
+    public List<Matrix4x4Serializable> matrixList = new List<Matrix4x4Serializable>();
     public Matrix4x4SerializableList() {}
 
     public Matrix4x4SerializableList(List<Matrix4x4> list)
     {
         foreach (Matrix4x4 matrix in list)
-            data.Add(new Matrix4x4Serializable(matrix));
+            matrixList.Add(new Matrix4x4Serializable(matrix));
     }
 }
